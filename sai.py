db1d94371f5aab523bcefce474b907fb6d184f9b
Teja <tejakondramutla021@gmail.com>
2025-05-23 12:39:09 +0530

    base program

--- sai.py
+++ sai.py
@@ -1,25 +1,20 @@
-x = 1    # int
-y = 2.8  # float
-z = 1j   # complex
+num = int(input("enter a any number between 1 to 7: "))
+if num==1:
+    print("sunday")
+elif num==2:
+    print("monday")
+elif num==3:
+    print("tuesday")
+elif num==4:
+    print("wednesday")
+elif num==5:
+    print("thursday")
+elif num==6:
+    print("friday")
+elif num==7:
+    print("sat")
+else:
+    print("its not weekend")
 
-#convert from int to float:
-a = float(x)
-
-#convert from float to int:
-b = int(y)
-
-#convert from int to complex:
-c = complex(x)
-
-a = int(4)
-b = int(5.0)
-c = int(6)
-print(a)
-print(b)
-print(c)
-
-x = float(5)
-y = float(6.9)
-print(x)
-print(y)
+9
 
